134a50eb66b1f99ba2788a8bf5cd0cf8fca75eb6
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-01 15:58:09 -0300

    main

--- index.js
+++ index.js
@@ -3,40 +3,40 @@ const path = require("path");
 const app = express();
 const port = (process.env.PORT || 8000); // Const para armanezar a porta do servidor
 
-let message = "";
+const Pokedex = [{
+  Número:"N°802",
+  Nome:"Marshadow",
+  Tipo:"Lutador, Fantasma",
+  Imagem:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/802.png",
+  Descrição:"It slips into the shadows of others and mimics their powers and movements",
+  Altura:"107 M",
+  Peso:"22.2Kg",
+  Categoria:"Gloomweller",
+  Habilidade:"Technician"},
+  {
 
-const Pokedex = [
-    {
-        Número:"N°802",
-        Nome:"Marshadow",
-        Tipo:"Lutador, Fantasma",
-        Imagem:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/802.png",
-        Descrição:"It slips into the shadows of others and mimics their powers and movements",
-        Altura:"107 M",
-        Peso:"22.2Kg",
-        Categoria:"Gloomweller",
-        Habilidade:"Technician"},
-        {
-            Número:"N°282",
-            Nome:"Gardevoir",
-            Tipo:"Psiquico, Fada",
-            Imagem:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/282.png",
-            Descrição:"It has the power to predict the future. Its power peaks when it is protecting its Trainer",
-            Altura:"1.6 M",
-            Peso:"48.4Kg",
-            Categoria:"Embrace",
-            Habilidade:"Synchronize"},
-            {
-                Número:"N°093",
-                Nome:"Haunter",
-                Tipo:"Fantasma, Venenoso",
-                Imagem:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/093.png",
-                Descrição:"Its tongue is made of gas. If licked, its victim starts shaking constantly until death eventually comes.",
-                Altura:"1.6 M",
-                Peso:"0.1Kg",
-                Categoria:"Gas",
-                Habilidade:"Levitate"}
+      Número:"N°282",
+      Nome:"Gardevoir",
+      Tipo:"Psiquico, Fada",
+      Imagem:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/282.png",
+      Descrição:"It has the power to predict the future. Its power peaks when it is protecting its Trainer",
+      Altura:"1.6 M",
+      Peso:"48.4Kg",
+      Categoria:"Embrace",
+      Habilidade:"Synchronize"},
+      {
+          Número:"N°093",
+          Nome:"Haunter",
+          Tipo:"Fantasma, Venenoso",
+          Imagem:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/093.png",
+          Descrição:"Its tongue is made of gas. If licked, its victim starts shaking constantly until death eventually comes.",
+          Altura:"1.6 M",
+          Peso:"0.1Kg",
+          Categoria:"Gas",
+          Habilidade:"Levitate"}
 ];
+
+let message = "";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -46,11 +46,15 @@ app.use(express.urlencoded());
     setTimeout(() => {
       message = "";
     }, 1000);
-    res.render("index", {Titulo: "poke", Pokedex : Pokedex, message}); // Nome do arquivo, o EJS já busca dentro da pasta views.
+    res.render("index", {Pokedex, message}); // Nome do arquivo, o EJS já busca dentro da pasta views.
   });
 
-  app.get("/detalhes", (req, res) => {
-    res.render("detalhes", {Titulo: "poke", Pokedex : Pokedex}); // Nome do arquivo, o EJS já busca dentro da pasta views.
+  app.get("/detalhes/:id", (req, res) => {
+    const id = req.params.id;
+    const pokemon = Pokedex[id];
+    res.render("detalhes", {
+      pokemon,
+    });
   });
 
   app.get("/cadastro", (req, res) => {
@@ -58,26 +62,10 @@ app.use(express.urlencoded());
   });
 
   app.post("/new", (req, res) => {
-    const {number,
-      nome,
-      type,
-      img,
-      descrypt,
-      alt,
-      kg,
-      cat,
-      ability} = req.body;
-    Pokedex.push({Número: number,
-      Nome: nome,
-      Tipo: type,
-      Imagem: img,
-      Descrição: descrypt,
-      Altura: alt,
-      Peso: kg,
-      Categoria: cat,
-      Habilidade: ability}); // Nome do arquivo, o EJS já busca dentro da pasta views.
-      message = "Seu pokémon foi cadastrado com sucesso!!!"
-      res.redirect("/")
+    const pokemon = req.body;
+    Pokedex.push(pokemon);
+    message = "Seu pokemon foi registrado!!!";
+    res.redirect("/");
   });
 
 // Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
